75641a8176ba8a1256466bc8c4358aea6faeaff0
Miguel Portero Ruiz <58771720+MigPorteroDevelop@users.noreply.github.com>
2024-06-09 23:09:15 +0200

    First commit -- Header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,57 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Filter from './components/Filter.vue';
+
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <header>
+      <h1>Expense planner</h1>
+    </header>
+    <!--<Filter />-->
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+:root {
+  --blue: #3b82f6;
+  --white: #FFF;
+  --light-gray: #F5F5F5;
+  --gray: #94a3b8;
+  --dark-gray: #64748b;
+  --black: #000;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+html {
+  font-size: 62.5%;
+  box-sizing: border-box;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+*,
+* :before,
+* :after {
+  box-sizing: inherit;
+}
+
+body {
+  font-size: 1.6rem;
+  font-family: "Lato";
+}
+
+h1 {
+  font-size: 4rem;
+}
+
+h2 {
+  font-size: 3rem;
+}
+header{
+  background-color: var(--blue);
+}
+header h1{
+  padding: 3rem 0;
+  margin: 0;
+  color: var(--white);
+  text-align: center;
 }
 </style>
